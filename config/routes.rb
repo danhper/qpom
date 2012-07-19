b4aeb46877ac81663e796d682a2009c727f6667c
furuyamasaru <yannwennri2@gmail.com>
2012-07-20 06:26:54 +0900

    update

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Qpom::Application.routes.draw do
   # first created -> highest priority.
   
   resources :top
+  resources :login
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
